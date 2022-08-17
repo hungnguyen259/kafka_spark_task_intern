e322ac6f5b08760015c3ab03279d49847de35830
hungnguyensv259 <hungnguyensv259@gmail.com>
2022-08-17 17:18:21 +0700

    Fix

---
+++ src/main/java/Kafka.java
@@ -0,0 +1,83 @@
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.sql.streaming.Trigger;
+
+import java.util.concurrent.TimeoutException;
+
+import static org.apache.spark.sql.functions.*;
+
+public class Kafka {
+    public static void main(String[] args) {
+        String kafkaServer = "10.3.68.20:9092, 10.3.68.21:9092, 10.3.68.23:9092, 10.3.68.26:9092, 10.3.68.28:9092, 10.3.68.32:9092, 10.3.68.47:9092, 10.3.68.48:9092, 10.3.68.50:9092, 10.3.68.52:9092";
+        String kafkaTopic = "rt-queue_1";
+        String savedDataLocation = "task_intern/data";
+        String checkpoint = "/tmp/task_intern";
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("kafka_tasks")
+                .getOrCreate();
+
+        Dataset<Row> data = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", kafkaServer)
+                .option("subscribe", kafkaTopic)
+                .load()
+                .selectExpr("CAST(value AS STRING) AS dataValue");
+        data.printSchema();
+
+        Dataset<Row> df = data
+                .withColumn("timeLog", split(col("dataValue"), "\t").getItem(0).cast("long"))
+                .withColumn("ip", split(col("dataValue"), "\t").getItem(1).cast("String"))
+                .withColumn("userAgent", split(col("dataValue"), "\t").getItem(2).cast("String"))
+                .withColumn("guidIime", split(col("dataValue"), "\t").getItem(3).cast("String"))
+                .withColumn("iditem", split(col("dataValue"), "\t").getItem(4).cast("String"))
+                .withColumn("viewCount", split(col("dataValue"), "\t").getItem(5).cast("int"))
+                .withColumn("guid", split(col("dataValue"), "\t").getItem(6).cast("String"))
+                .withColumn("domain", split(col("dataValue"), "\t").getItem(7).cast("String"))
+                .withColumn("tp", split(col("dataValue"), "\t").getItem(8).cast("long"))
+                .withColumn("cov", split(col("dataValue"), "\t").getItem(9).cast("long"))
+                .withColumn("zoneId", split(col("dataValue"), "\t").getItem(10).cast("long"))
+                .withColumn("campaign", split(col("dataValue"), "\t").getItem(11).cast("String"))
+                .withColumn("channelId", split(col("dataValue"), "\t").getItem(12).cast("long"))
+                .withColumn("guidIsNew", split(col("dataValue"), "\t").getItem(13).cast("long"))
+                .withColumn("referer", split(col("dataValue"), "\t").getItem(14).cast("String"))
+                .withColumn("location", split(col("dataValue"), "\t").getItem(15).cast("int"))
+                .withColumn("transactionId", split(col("dataValue"), "\t").getItem(16).cast("String"))
+                .withColumn("lsn", split(col("dataValue"), "\t").getItem(17).cast("int"))
+                .withColumn("iteminfo", split(col("dataValue"), "\t").getItem(18).cast("int"))
+                .withColumn("tpn", split(col("dataValue"), "\t").getItem(19).cast("int"))
+                .withColumn("rid", split(col("dataValue"), "\t").getItem(20).cast("String"))
+                .withColumn("sspz", split(col("dataValue"), "\t").getItem(21).cast("long"))
+                .withColumn("adc_cpa", split(col("dataValue"), "\t").getItem(22).cast("String"))
+                .withColumn("pgl", split(col("dataValue"), "\t").getItem(23).cast("int"))
+                .withColumn("dl", split(col("dataValue"), "\t").getItem(24).cast("int"))
+                .withColumn("date", from_unixtime(col("timeLog"), "MM-dd-yyyy"))
+                .withColumn("year", split(col("date"), "-").getItem(2).cast("int"))
+                .withColumn("month", split(col("date"), "-").getItem(0).cast("int"))
+                .withColumn("day", split(col("date"), "-").getItem(1).cast("int"))
+                .drop("timeLog")
+                .drop("date")
+                .drop("dataValue");
+
+
+        try {
+            df.writeStream()
+                    .format("parquet")
+                    .outputMode("append")
+                    .option("path", savedDataLocation)
+                    .option("header", true)
+                    .option("checkpointLocation", checkpoint)
+                    .trigger(Trigger.ProcessingTime("3 minute"))
+                    .partitionBy("year", "month", "day")
+                    .start()
+                    .awaitTermination();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (StreamingQueryException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,19 +1,22 @@
 import org.apache.spark.sql.*;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.sql.streaming.Trigger;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
+import java.util.concurrent.TimeoutException;
+
 import static org.apache.spark.sql.functions.*;
 
 public class Task {
-
     public static void main(String[] args) {
 
         StructType schema = DataTypes.createStructType(new StructField[] {
-                DataTypes.createStructField("campaign", DataTypes.StringType, true),
-                DataTypes.createStructField("cov", DataTypes.LongType, true),
+                DataTypes.createStructField("campaign", DataTypes.IntegerType, true),
+                DataTypes.createStructField("cov", DataTypes.IntegerType, true),
                 DataTypes.createStructField("location", DataTypes.IntegerType, true),
-                DataTypes.createStructField("guid", DataTypes.StringType, true) }
+                DataTypes.createStructField("guid", DataTypes.IntegerType, true) }
         );
 
         SparkSession spark = SparkSession
@@ -21,9 +24,9 @@ public class Task {
                 .appName("spark tasks")
                 .getOrCreate();
 
-        String sourceFile = "test2";
-        String resultFolder = "resulttest2";
-
+        String sourceFile = "task_intern/data";
+        String resultFolder = "task_intern/result";
+        String checkpoint ="tmp/task_intern";
         Dataset<Row> df = spark
                 .readStream()
                 .schema(schema)
@@ -31,9 +34,32 @@ public class Task {
                 .parquet(sourceFile)
                 .select("campaign", "cov", "location", "guid", "year", "month", "day")
                 .withColumn("date", expr("make_date(year, month, day)"));
-        df.show();
-//
-////        Số lượng click, view ứng với mỗi campaign
+
+
+//        Số lượng click, view ứng với mỗi campaign
+        Dataset<Row> df1 = df.groupBy("date", "campaign", "cov").count();
+        Dataset<Row> df2 = df1.groupBy("date", "campaign").agg(sum("count").as("sum"));
+        df1.createOrReplaceTempView("df1");
+        df2.createOrReplaceTempView("df2");
+        Dataset<Row> ex1 = spark.sql("select df2.date, df2.campaign, ifnull(df1.count, 0) as view, ifnull(df2.sum-df1.count, sum) as click " +
+                "from df1 right join df2 " +
+                "on df1.date=df2.date and df1.campaign=df2.campaign and df1.cov=0 " +
+                "order by df2.date desc, df2.campaign desc");
+        ex1.write().option("delimiter", ";").option("header", "true").mode(SaveMode.Overwrite).csv(resultFolder + "/resultExercise1");
+
+
+//        Tìm số lượng click, tỉ lệ view ứng với mỗi campaign theo location
+        Dataset<Row> df3 = df.groupBy("date", "location", "campaign", "cov").count();
+        Dataset<Row> df4 = df3.groupBy("date", "location", "campaign").agg(sum("count").as("sum"));
+        df3.createOrReplaceTempView("df3");
+        df4.createOrReplaceTempView("df4");
+        Dataset<Row> ex2 = spark.sql("select df4.date, df4.location, df4.campaign, df4.sum, ifnull(df3.count, 0) as view, ifnull(df4.sum-df3.count, sum) as click " +
+                "from df3 right join df4 " +
+                "on df4.date=df3.date and df3.campaign=df4.campaign and df3.location=df4.location and df3.cov=0 " +
+                "order by df4.date desc, df4.location desc, df4.campaign desc");
+        ex2.write().option("delimiter", ";").option("header", "true").mode(SaveMode.Overwrite).csv(resultFolder + "/resultExercise2");
+
+//        Tìm tỉ lệ click, tỉ lệ view ứng vỡi mỗi campaign
 //        Dataset<Row> df1 = df.groupBy("date", "campaign", "cov").count();
 //        Dataset<Row> df2 = df1.groupBy("date", "campaign").agg(sum("count").as("sum"));
 //        df1.createOrReplaceTempView("df1");
@@ -41,64 +67,97 @@ public class Task {
 //        Dataset<Row> ex1 = spark.sql("select df2.date, df2.campaign, ifnull(df1.count, 0) as view, ifnull(df2.sum-df1.count, sum) as click " +
 //                "from df1 right join df2 " +
 //                "on df1.date=df2.date and df1.campaign=df2.campaign and df1.cov=0 " +
-//                "order by df2.date desc, df2.campaign desc");
+//                "order by df1.date desc, df2.campaign desc");
 //        ex1.show();
-//        ex1.write().option("delimiter", ";").option("header", "true").mode(SaveMode.Overwrite).csv(resultFolder + "/resultExercise1");
-//
-//
-////        Tìm số lượng click, tỉ lệ view ứng với mỗi campaign theo location
+
+//        Tìm tỉ lệ click, tỉ lệ view ứng với mỗi campaign theo location
 //        Dataset<Row> df3 = df.groupBy("date", "location", "campaign", "cov").count();
 //        Dataset<Row> df4 = df3.groupBy("date", "location", "campaign").agg(sum("count").as("sum"));
 //        df3.createOrReplaceTempView("df3");
 //        df4.createOrReplaceTempView("df4");
-//        Dataset<Row> ex2 = spark.sql("select df4.date, df4.location, df4.campaign, df4.sum, ifnull(df3.count, 0) as view, ifnull(df4.sum-df3.count, sum) as click " +
-//                "from df3 right join df4 " +
-//                "on df4.date=df3.date and df3.campaign=df4.campaign and df3.location=df4.location and df3.cov=0 " +
-//                "order by df4.date desc, df4.location desc, df4.campaign desc");
-//        ex2.show();
-//        ex2.write().option("delimiter", ";").option("header", "true").mode(SaveMode.Overwrite).csv(resultFolder + "/resultExercise2");
-//
-////        Tìm tỉ lệ click, tỉ lệ view ứng vỡi mỗi campaign
-////        Dataset<Row> df1 = df.groupBy("date", "campaign", "cov").count();
-////        Dataset<Row> df2 = df1.groupBy("date", "campaign").agg(sum("count").as("sum"));
-////        df1.createOrReplaceTempView("df1");
-////        df2.createOrReplaceTempView("df2");
-////        Dataset<Row> ex1 = spark.sql("select df2.date, df2.campaign, ifnull(df1.count, 0) as view, ifnull(df2.sum-df1.count, sum) as click " +
-////                "from df1 right join df2 " +
-////                "on df1.date=df2.date and df1.campaign=df2.campaign and df1.cov=0 " +
-////                "order by df1.date desc, df2.campaign desc");
-////        ex1.show();
-//
-////        Tìm tỉ lệ click, tỉ lệ view ứng với mỗi campaign theo location
-////        Dataset<Row> df3 = df.groupBy("date", "location", "campaign", "cov").count();
-////        Dataset<Row> df4 = df3.groupBy("date", "location", "campaign").agg(sum("count").as("sum"));
-////        df3.createOrReplaceTempView("df3");
-////        df4.createOrReplaceTempView("df4");
-////        Dataset<Row> ex5 = spark.sql("select df4.date, df4.campaign, df4.location, df4.sum, ifnull(df3.count, 0) as view, ifnull(df4.sum-df3.count, sum) as click " +
-////                        "from df3 right join df4 " +
-////                        "on df4.date=df3.date and df3.campaign=df4.campaign and df3.location=df4.location and df3.cov=0 " +
-////                        "order by df3.date desc, df3.location desc, df3.campaign desc")
-////                .withColumn("rateView", col("view").divide(col("sum")))
-////                .withColumn("rateClick", col("click").divide(col("sum")));
-////        ex5.show();
-//
-////      Số lượng user truy cập ứng với mỗi campaign
-//
-//        Dataset<Row> ex3 = df.groupBy("date", "campaign")
-//                .agg(countDistinct("guid").alias("count")).
-//                orderBy(col("date").desc(), col("campaign").desc());
-//        ex3.show();
-//        ex3.write().option("delimiter", ";").option("header", "true").mode(SaveMode.Overwrite).csv(resultFolder + "/resultExercise3");
-//
-////      Số lượng user truy cập nhiều hơn một campaign
-//        Dataset<Row> ex4 = df.groupBy("date", "guid")
-//                .agg(count("campaign").as("count"))
-//                .filter("count>1")
-//                .groupBy("date").agg(count("guid"))
-//                .orderBy(col("date").desc());
-//        ex4.show();
-//        ex4.write().option("delimiter", ";").option("header", "true").mode(SaveMode.Overwrite).csv(resultFolder + "/resultExercise4");
-//
+//        Dataset<Row> ex5 = spark.sql("select df4.date, df4.campaign, df4.location, df4.sum, ifnull(df3.count, 0) as view, ifnull(df4.sum-df3.count, sum) as click " +
+//                        "from df3 right join df4 " +
+//                        "on df4.date=df3.date and df3.campaign=df4.campaign and df3.location=df4.location and df3.cov=0 " +
+//                        "order by df3.date desc, df3.location desc, df3.campaign desc")
+//                .withColumn("rateView", col("view").divide(col("sum")))
+//                .withColumn("rateClick", col("click").divide(col("sum")));
+//        ex5.show();
+
+//      Số lượng user truy cập ứng với mỗi campaign
+
+        Dataset<Row> ex3 = df.groupBy("date", "campaign")
+                .agg(countDistinct("guid").alias("count")).
+                orderBy(col("date").desc(), col("campaign").desc());
+        ex3.write().option("delimiter", ";").option("header", "true").mode(SaveMode.Overwrite).csv(resultFolder + "/resultExercise3");
+
+//      Số lượng user truy cập nhiều hơn một campaign
+        Dataset<Row> ex4 = df.groupBy("date", "guid")
+                .agg(count("campaign").as("count"))
+                .filter("count>1")
+                .groupBy("date").agg(count("guid"))
+                .orderBy(col("date").desc());
+        ex4.write().option("delimiter", ";").option("header", "true").mode(SaveMode.Overwrite).csv(resultFolder + "/resultExercise4");
+
+        try{
+            ex1.writeStream()
+                    .format("csv")
+                    .outputMode("append")
+                    .option("path", resultFolder + "/ex1")
+                    .option("header", true)
+                    .option("checkpointLocation", checkpoint+ "/ex1")
+                    .trigger(Trigger.ProcessingTime("60 seconds"))
+                    .partitionBy("year", "month", "day")
+                    .start()
+                    .awaitTermination();
+        }
+        catch(TimeoutException | StreamingQueryException e){
+            e.printStackTrace();
+        }
+        try{
+            ex2.writeStream()
+                    .format("csv")
+                    .outputMode("append")
+                    .option("path", resultFolder + "/ex2")
+                    .option("header", true)
+                    .option("checkpointLocation", checkpoint+ "/ex2")
+                    .trigger(Trigger.ProcessingTime("60 seconds"))
+                    .partitionBy("year", "month", "day")
+                    .start()
+                    .awaitTermination();
+        }
+        catch(TimeoutException | StreamingQueryException e){
+            e.printStackTrace();
+        }
+        try{
+            ex3.writeStream()
+                    .format("csv")
+                    .outputMode("append")
+                    .option("path", resultFolder + "/ex3")
+                    .option("header", true)
+                    .option("checkpointLocation", checkpoint + "/ex3")
+                    .trigger(Trigger.ProcessingTime("60 seconds"))
+                    .partitionBy("year", "month", "day")
+                    .start()
+                    .awaitTermination();
+        }
+        catch(TimeoutException | StreamingQueryException e){
+            e.printStackTrace();
+        }
+        try{
+            ex4.writeStream()
+                    .format("csv")
+                    .outputMode("append")
+                    .option("path", resultFolder + "/ex4")
+                    .option("header", true)
+                    .option("checkpointLocation", checkpoint+ "/ex4")
+                    .trigger(Trigger.ProcessingTime("60 seconds"))
+                    .partitionBy("year", "month", "day")
+                    .start()
+                    .awaitTermination();
+        }
+        catch(TimeoutException | StreamingQueryException e){
+            e.printStackTrace();
+        }
     }
 }
 
